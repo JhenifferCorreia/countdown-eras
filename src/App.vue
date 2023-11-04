5f501cbfeb5035824af051e6ab5313e58f6a7065
Jhen <jhencorreia@gmail.com>
2023-11-04 19:17:09 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Countdown from './components/Countdown.vue'
+</script>
+
+<template>
+  <div><img src="./assets/images/the-eras-tour.png" alt="Logo The Eras Tour"></div>
+  <Countdown />
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+img {
+  width: clamp(200px, 50vw, 500px);
+  margin-bottom: 3.5em;
+}
+</style>
---
+++ src/components/Countdown.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const days = ref(0);
+const hours = ref(0);
+const minutes = ref(0);
+const seconds = ref(0);
+const countDownDate = ref(new Date("2023-11-17").getTime());
+
+onMounted(() => {
+  getTime();
+});
+
+function getTime() {
+  CountDown();
+}
+
+function CountDown() {
+  setInterval(() => {
+    const now = new Date().getTime();
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+    const nextDay = new Date(countDownDate.value);
+    nextDay.setDate(nextDay.getDate() + 1);
+
+    const distance = Number(nextDay) - now;
+
+    days.value = Math.floor(distance / (1000 * 60 * 60 * 24));
+    hours.value = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes.value = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    seconds.value = Math.floor((distance % (1000 * 60)) / 1000);
+  }, 1000);
+}
+
+function updateCountdown(dateString: string) {
+  countDownDate.value = new Date(dateString)!.getTime();
+}
+
+function formatedDays() {
+  return String(days.value).padStart(2, '0');
+}
+
+function formatedHours() {
+  return String(hours.value).padStart(2, '0');
+}
+
+function formatedMinutes() {
+  return String(minutes.value).padStart(2, '0');
+}
+
+function formatedSeconds() {
+  return String(seconds.value).padStart(2, '0');
+}
+
+
+</script>
+
+<template>
+  <div>
+    <div class="countdown">
+      <p>{{ formatedDays() }}:{{ formatedHours() }}:{{ formatedMinutes() }}:{{ formatedSeconds() }}</p>
+    </div>
+
+    <div>
+      <div class="rj">
+        <button id="fearless" @click="updateCountdown('2023-11-17')">RJ 17/11</button>
+        <button id="red" @click="updateCountdown('2023-11-18')">RJ 18/11</button>
+        <button id="reputation" @click="updateCountdown('2023-11-19')">RJ 19/11</button>
+      </div>
+      <div class="sp">
+        <button id="evermore" @click="updateCountdown('2023-11-24')">SP 24/11</button>
+        <button id="lover" @click="updateCountdown('2023-11-25')">SP 25/11</button>
+        <button id="midnights" @click="updateCountdown('2023-11-26')">SP 26/11</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+p {
+  font-family: "Pistilli-Roman";
+  font-size: clamp(3rem, 0.2782rem + 4.8387vw, 3.375rem);
+}
+
+.countdown {
+
+  margin-bottom: 3.5em;
+}
+
+
+#fearless {
+  background-color: #F2C483;
+}
+
+#red {
+  background-color: #813C48;
+}
+
+#reputation {
+  background-color: #767173;
+}
+
+#evermore {
+  background-color: #C8B095;
+}
+
+#lover {
+  background-color: #F7B2CD;
+}
+
+#midnights {
+  background-color: #303850;
+}
+</style>
